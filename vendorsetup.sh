66fb6c6906a5139a048f0c0f64671b2e7008ecdf
Onelots <onelots@onelots.fr>
2025-04-28 03:31:04 +0200

    vendorsetup: drop useless dependencies
    Signed-off-by: Onelots <onelots@onelots.fr>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,3 @@
-echo "Cloning Audio Configs...."
-# Clone own audio config tree
-rm -rf hardware/qcom-caf/sm8350/audio/configs/holi && git clone https://github.com/Amrito-Projects/hardware_qcom-caf_sm8350_audio_configs_holi.git -b 14 hardware/qcom-caf/sm8350/audio/configs/holi --depth=1
-
-echo "Cloning vendor..."
-# Clone Vendor
-git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi_veux-new.git -b 15 vendor/xiaomi/veux
-
-echo "Cloning hardware related stuff..."
-# Hardware
-git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.1 hardware/xiaomi
-
 echo "Cloning Kernel"
 # Kernel
 git clone --depth=1 https://github.com/PaimonLab/kernel_xiaomi_veux.git kernel/xiaomi/veux
